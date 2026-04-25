76391469fcbc351aa176cf13463cedb1c7a28926
Matthieu Gagné <mathdaman@gmail.com>
2026-04-25 15:23:01 -0400

    feat: wire workshop.gd — single right-wall door to cantina

--- scripts/areas/workshop.gd
+++ scripts/areas/workshop.gd
@@ -2,14 +2,9 @@
 extends Node2D
 
 @onready var cantina_door: Area2D = $CantinaDoor
-@onready var quarters_door: Area2D = $QuartersDoor
 
 func _ready() -> void:
 	cantina_door.body_entered.connect(func(body):
 		if body.is_in_group("player"):
 			get_tree().get_root().get_node("Main").go_to_area("cantina")
-	)
-	quarters_door.body_entered.connect(func(body):
-		if body.is_in_group("player"):
-			get_tree().get_root().get_node("Main").go_to_area("quarters")
 	)
