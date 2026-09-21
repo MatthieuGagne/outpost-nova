d444fd41d48df88c52528cb7c80b413031a5c283
MATTHIEU GAGNÉ <mathdaman@gmail.com>
2026-09-21 17:43:59 -0400

    feat: delete the 2D workshop and its orphaned workbench

--- scripts/workbench.gd
+++
@@ -1,8 +0,0 @@
-# scripts/workbench.gd
-extends StaticBody2D
-
-func _ready() -> void:
-	add_to_group("interactable")
-
-func interact() -> void:
-	get_tree().get_root().get_node("Main").open_crafting()
